bea9d3043a1a05ac46ff9e92e76bb98d6a2fe1af
SofiSantoro <sofiaasantoro@gmail.com>
2021-07-04 16:48:42 -0300

    primer ejercicio JS

---
+++ HOLA/index.js
@@ -0,0 +1,17 @@
+/*let nombre = prompt("¿Cuál es tu nombre?");
+let apellido = prompt("¿Cuál es tu apellido?");
+
+let respuestaDelUsuario = (nombre + " " + apellido);
+
+alert(`Hola ${respuestaDelUsuario}, bienvenida a Ada`);
+
+console.log(respuestaDelUsuario);*/
+
+let nombre = prompt("¿Cuál es tu nombre?");
+let apellido = prompt("¿Cuál es tu apellido?");
+
+let respuestaDelUsuario = (nombre + " " + apellido);
+
+alert(`Hola ` + respuestaDelUsuario + `, bienvenida a Ada`);
+
+console.log(respuestaDelUsuario);
